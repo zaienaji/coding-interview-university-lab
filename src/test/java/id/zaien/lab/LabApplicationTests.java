ea6b1670103b6b6a71003c0108d2f961f8fcbe8e
zaienaji <zaien.aji@gmail.com>
2022-12-29 19:53:05 +0800

    init

---
+++ src/test/java/id/zaien/lab/LabApplicationTests.java
@@ -0,0 +1,13 @@
+package id.zaien.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
