289e1ce7fe7c2ae816c41cf9a0087fe9477da64f
Zaien Aji Trahutomo <zaien.aji@gmail.com>
2022-12-30 09:52:43 +0800

    add unit test to home controller

--- src/test/java/id/zaien/lab/LabApplicationTests.java
+++ src/test/java/id/zaien/lab/LabApplicationTests.java
@@ -1,13 +1,20 @@
 package id.zaien.lab;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LabApplicationTests {
 
+	@Autowired
+	private HelloController sut;
+	
 	@Test
 	void contextLoads() {
+		assertThat(sut).isNotNull();
 	}
 
 }
